7da001058d3d80c4ea96ff4a16c2c5f4778fc10c
Brian <briantaehwang@gmail.com>
2024-04-19 15:44:59 -0700

    GameObject diversified into child classes to handle all behavior/interactions rather than Board class

--- game.py
+++ game.py
@@ -16,19 +16,20 @@ class GameObject:
 
     def __init__(self, symbol):
         self.symbol = symbol
-        self.discovered = False
-        self.captured = False
         self.occupied = symbol != '.'
-
-    def reset(self):
-        self.symbol = '.'
-        self.discovered = False
-        self.captured = False
-        self.occupied = False
+        
+   
 
 #This is a "Fire" class - a child of GameObject 
-class Fire(GameObject):
+
+class Elements(GameObject):
+    elem_rand_multiplier_bounds = (1,4)
+    discovered = False
+    captured = None
+
+class Fire(Elements):
     debugName = 'Fire'
+    elem_rand_multiplier_bounds = (1,1)
     def __init__(self):
         #This is a call to the parent class constructor
         super().__init__('F')
@@ -45,115 +46,53 @@ class Board:
     def __init__(self, rows, cols):
         self.rows = rows
         self.cols = cols
-        # self.p1_row = 0
-        # self.p1_col = 0
-        # self.p2_row = 1
-        # self.p2_col = 1
-        # self.p1_pos = (self.p1_row, self.p1_col)
-        # self.p2_pos = (self.p2_row, self.p2_col)
         self.all_elem_pos = set()
         self.grid = [[None for _ in range(self.cols)] for _ in range(self.rows)]
         print(f'Created a Board instance with dimensions {rows} x {cols}')
 
-    def genBoard(self, elem_types):
-        self.genElems(elem_types)
+    def genBoard(self, elemTypes):
+        self.genElems(elemTypes)
 
-    # def genRandBoard(self):
-    #     relem_type_total = 
-    #     relem_total = 
-    #     robstacle_total = 
-    #     robstacle_type_total = 
-        # self.genElem(relem_type_total, relem_total)
-        # self.genObstacle(robstacle_type_total, robstacle_total)
     def clearBoard(self):
         for row in self.grid:
-            for currGO in row:
-                currGO.reset()
+            for current_game_object in row:
+                current_game_object.reset()
+
     def getObj(self, row, col):
         return self.grid[row][col]
+    
     def drawBoard(self):
         for row in self.grid:
-            rowToPrint = ''
+            row_to_print = ''
             for entry in row:
                 if entry is not None:
                     #If the entry is not empty, append that game object's symbol
-                    rowToPrint = rowToPrint + entry.symbol
+                    row_to_print = row_to_print + entry.symbol
                 else:
                     #Otherwise, put the default "Empty" symbol
-                    rowToPrint = rowToPrint + self.emptySymbol
-            print(rowToPrint)
+                    row_to_print = row_to_print + self.emptySymbol
+            print(row_to_print)
 
-    def genElems(self, elem_types):
+    def genElems(self, elemTypes):
         print("\n--- Starting genElems ---")
-        print(f'Input elem_types: {elem_types}')
-
-        """
-        Leaving this code for you to reference in case you weren't done with it - 
-
-
-        elem_types = set(elem_types)
-        included_elems = {
-            'water': False,
-            'earth': False,
-            'fire': False,
-            'air': False,
-        }
-        included_elems_symbols = {
-            0: 'W',
-            1: 'E',
-            2: 'F',
-            3: 'A',
-        }
+        print(f'Input elem_types: {elemTypes}')
         
-        for elem in elem_types:
-            if elem == 0:
-                included_elems['water'] = True
-            elif elem == 1:
-                included_elems['earth'] = True
-            elif elem == 2:
-                included_elems['fire'] = True
-            elif elem == 3:
-                included_elems['air'] = True
-            
-            elem_gameObject = GameObject(included_elems_symbols[elem])
-            print(f'Processing element type {elem} with symbol: {elem_gameObject.symbol}')
-            
-            rand_multiplier = random.randint(1, 3)
-            print(f'Multiplier for element type {elem}: {rand_multiplier}')
-            
-            for _ in range(rand_multiplier): 
-                while True:
-                    rand_row = random.randint(0, self.rows - 1)
-                    rand_col = random.randint(0, self.cols - 1)
-                    rand_pos = (rand_row, rand_col)
-                    
-                    if rand_pos not in self.all_elem_pos and self.grid[rand_pos[0]][rand_pos[1]]== None:
-                        self.all_elem_pos.add(rand_pos)
-                        self.grid[rand_pos[0]][rand_pos[1]] = elem_gameObject
-                        break
-                
-                print(f'Placed element type {elem} at position {rand_pos}')
-            
-            print(f'Current element positions: {self.all_elem_pos}')
-            
-        # print("--- Finished genElems ---\n")
-        """
-        
-        if type(elem_types) != list:
+        if type(elemTypes) != list:
             #This is a check for if a singleton argument is passed in instead of a list
-            elem_types = [elem_types]
+            elemTypes = [elemTypes]
         #Converting to a set removes duplicates
-        elem_types = list(set(elem_types))
-        for elem in elem_types:
-            print(f'Processing element type {elem().debugName} with symbol: {elem().symbol}')
-            rand_multiplier = random.randint(1, 3)
-            print(f'Multiplier for element type {elem().debugName}: {rand_multiplier}')
+        elemTypes = list(set(elemTypes))
+        for elem in elemTypes:
+            ref_class_inst = elem()
+            print(f'Processing element type {ref_class_inst.debugName} with symbol: {ref_class_inst.symbol}')
+            rand_multiplier = random.randint(ref_class_inst.elem_rand_multiplier_bounds[0], ref_class_inst.elem_rand_multiplier_bounds[1])
+            print(f'Multiplier for element type {ref_class_inst.debugName}: {rand_multiplier}')
             
             for _ in range(rand_multiplier): 
                 while True:
                     rand_pos = (random.randint(0, self.rows - 1), random.randint(0, self.cols - 1))
                     
-                    if rand_pos not in self.all_elem_pos and self.getObj(rand_pos[0],rand_pos[1] )== None:
+                    if rand_pos not in self.all_elem_pos and self.getObj(rand_pos[0],rand_pos[1]) == None:
                         self.all_elem_pos.add(rand_pos)
                         self.grid[rand_pos[0]][rand_pos[1]] = elem()
                         break
@@ -167,8 +106,6 @@ class Board:
 
 
 
-    
-    
     # def boardStats(self):
                 
 
@@ -178,6 +115,7 @@ class Board:
 print("Constructing Gameboard as gb...")
 gb = Board(10, 10)
 gb.drawBoard()
+gb.genBoard(Fire)
 
 
        
@@ -214,8 +152,6 @@ gb.drawBoard()
 # class Unit:
 #     """
 #     Manager class which oversees turns and game
-
-
 #     """  
 
 
