350198b796b30870d89836f97d485139a340ac6a
Brian <briantaehwang@gmail.com>
2024-04-19 10:23:39 -0700

    For source control, currently working on procedural generation of elements in board

--- game.py
+++ game.py
@@ -3,7 +3,7 @@
 
 #Python script to define Game, Unit, Agent, and Board sub classes
 
-
+import random
 
 
 
@@ -37,12 +37,115 @@ class Game:
    """
 
 
+class GameObject:
+    """
+    In-game object class that manages symbols on the board representing agents,
+    elements, obstacles, and movable points of space. It also tracks the statuses
+    of elements (undiscovered vs. discovered, uncaptured vs. captured).
+    """
+    occupied = False
+
+    def __init__(self, symbol):
+        self.symbol = symbol
+        self.discovered = False
+        self.captured = False
+        self.occupied = symbol != '.'
+
+    def reset(self):
+        self.symbol = '.'
+        self.discovered = False
+        self.captured = False
+        self.occupied = False
+
+
 class Board:
     """
     Board class which stores the map representation
-    genBoard() - Procedural generation of game board at inital state
+    genBoard() - Procedural generation of game board at initial state
     genBoardForAgent() - Create a copy of the game board for the agent
     """
+    
+    def __init__(self, rows, cols):
+        self.rows = rows
+        self.cols = cols
+        # self.p1_row = 0
+        # self.p1_col = 0
+        # self.p2_row = 1
+        # self.p2_col = 1
+        # self.p1_pos = (self.p1_row, self.p1_col)
+        # self.p2_pos = (self.p2_row, self.p2_col)
+        self.grid = [[GameObject('.') for _ in range(self.cols)] for _ in range(self.rows)]
+
+    def genBoard(self, elem_types):
+        self.genElems(elem_types)
+
+    # def genRandBoard(self):
+    #     relem_type_total = 
+    #     relem_total = 
+    #     robstacle_total = 
+    #     robstacle_type_total = 
+        # self.genElem(relem_type_total, relem_total)
+        # self.genObstacle(robstacle_type_total, robstacle_total)
+    def clearBoard(self):
+        for row in self.grid:
+            for currGO in row:
+                currGO.reset()
+
+    def drawBoard(self):
+        for row in self.grid:
+            rowtoprint = ''.join(currGO.symbol for currGO in row)
+            print(rowtoprint)
+
+    def genElems(self, elem_types): 
+        elem_types = set(elem_types)
+        included_elems = {
+            'water': False,
+            'earth': False,
+            'fire': False,
+            'air': False,
+        }
+        included_elems_symbols = {
+            0: 'W',
+            1: 'E',
+            2: 'F',
+            3: 'A',
+        }
+        for elem in elem_types:
+            if elem == 0:
+                included_elems['water'] = True
+            elif elem == 1:
+                included_elems['earth'] = True
+            elif elem == 2:
+                included_elems['fire'] = True
+            elif elem == 3:
+                included_elems['air'] = True
+            elem_symbol = GameObject(included_elems_symbols[elem])
+            all_elem_pos = set()
+            rand_multiplier = random.randint(1,5)
+            for _ in range(rand_multiplier): 
+                while True:
+                    rand_row = random.randint(0, self.rows - 1)
+                    rand_col = random.randint(0, self.cols - 1)
+                    rand_pos = (rand_row, rand_col)
+                    
+                    if rand_pos not in all_elem_pos and self.grid[rand_pos[0]][rand_pos[1]].occupied == False:
+                        all_elem_pos.add(rand_pos)
+                        self.grid[rand_pos[0]][rand_pos[1]].symbol = elem_symbol
+                        break
+
+
+                
+
+        
+
+           
+
+              
+              
+
+
+       
+
 
 
 class Unit:
@@ -51,3 +154,11 @@ class Unit:
 
 
     """  
+
+
+
+
+
+
+
+
