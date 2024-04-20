507a92a5a4a7b73fe2e0b3f7a0a9859f71064b51
Brian <briantaehwang@gmail.com>
2024-04-19 17:15:06 -0700

    reformatted whitespace/indentation

--- game.py
+++ game.py
@@ -1,40 +1,70 @@
-
-
-
-#Python script to define Game, Unit, Agent, and Board sub classes
-
 import random
 
 
 class GameObject:
     """
-    In-game object class that manages symbols on the board representing agents,
-    elements, obstacles, and movable points of space. It also tracks the statuses
-    of elements (undiscovered vs. discovered, uncaptured vs. captured).
+    In-game object class that represents a generic "game object" from which child classes
+    for elements, obstacles, units manage corresponding symbols, behavior/interactions. 
     """
     occupied = False
 
     def __init__(self, symbol):
         self.symbol = symbol
         self.occupied = symbol != '.'
-        
-   
 
-#This is a "Fire" class - a child of GameObject 
 
 class Elements(GameObject):
-    elem_rand_multiplier_bounds = (1,4)
-    discovered = False
-    captured = None
+    elem_rand_multiplier_bounds = (1, 4)
+    
+
+class Terrain(GameObject):
+    placeHolder = None
+
+class Water(Elements):
+    debugName = 'Water'
+    def __init__(self):
+        super().__init__('W')
+
+
+class Earth(Elements):
+    debugName = 'Earth'
+    def __init__(self):
+        super().__init__('E')
+
 
 class Fire(Elements):
     debugName = 'Fire'
-    elem_rand_multiplier_bounds = (1,1)
     def __init__(self):
-        #This is a call to the parent class constructor
         super().__init__('F')
-        #The terminology "Super" here is referring to the "Super Class", GameObject
-        #In this context, Fire is a subclass of the superclass GameObject
+
+
+class Air(Elements):
+    debugName = 'Air'
+    def __init__(self):
+        super().__init__('A')
+
+
+class Obstacles(Terrain):
+    def __init__(self, obstacleScale):
+        super().__init__('[]')
+        self.obstacleScale = obstacleScale
+    """
+    Shape generation
+        Randomly select a grid area on the board for obstacle generation
+        Determine which positions are already occupied by a instance of GameObject
+        Randomly select positions within grid area not already occupied
+        Store positions in a set
+    Placement
+        Send stored positions set to Board class for placement
+    
+    Choose seed which contains a class containing a function for "growing" from the seed
+    
+    """
+
+
+class Surface(Terrain):
+    placeHolder = None
+
 
 class Board:
     """
@@ -43,6 +73,7 @@ class Board:
     genBoardForAgent() - Create a copy of the game board for the agent
     """
     emptySymbol = '.'
+
     def __init__(self, rows, cols):
         self.rows = rows
         self.cols = cols
@@ -58,45 +89,41 @@ class Board:
             for current_game_object in row:
                 current_game_object.reset()
 
-    def getObj(self, row, col):
+    def getObject(self, row, col):
         return self.grid[row][col]
-    
+
     def drawBoard(self):
         for row in self.grid:
             row_to_print = ''
             for entry in row:
                 if entry is not None:
-                    #If the entry is not empty, append that game object's symbol
-                    row_to_print = row_to_print + entry.symbol
+                    row_to_print += entry.symbol
                 else:
-                    #Otherwise, put the default "Empty" symbol
-                    row_to_print = row_to_print + self.emptySymbol
+                    row_to_print += self.emptySymbol
             print(row_to_print)
 
     def genElems(self, elemTypes):
         print("\n--- Starting genElems ---")
         print(f'Input elem_types: {elemTypes}')
         
-        if type(elemTypes) != list:
-            #This is a check for if a singleton argument is passed in instead of a list
+        if not isinstance(elemTypes, list):
             elemTypes = [elemTypes]
-        #Converting to a set removes duplicates
         elemTypes = list(set(elemTypes))
+
         for elem in elemTypes:
             ref_class_inst = elem()
             print(f'Processing element type {ref_class_inst.debugName} with symbol: {ref_class_inst.symbol}')
             rand_multiplier = random.randint(ref_class_inst.elem_rand_multiplier_bounds[0], ref_class_inst.elem_rand_multiplier_bounds[1])
             print(f'Multiplier for element type {ref_class_inst.debugName}: {rand_multiplier}')
-            
+
             for _ in range(rand_multiplier): 
                 while True:
                     rand_pos = (random.randint(0, self.rows - 1), random.randint(0, self.cols - 1))
                     
-                    if rand_pos not in self.all_elem_pos and self.getObj(rand_pos[0],rand_pos[1]) == None:
+                    if rand_pos not in self.all_elem_pos and self.getObject(rand_pos[0], rand_pos[1]) is None:
                         self.all_elem_pos.add(rand_pos)
                         self.grid[rand_pos[0]][rand_pos[1]] = elem()
                         break
-                        
                 
                 print(f'Placed element type {elem().symbol} at position {rand_pos}')
             
@@ -115,7 +142,8 @@ class Board:
 print("Constructing Gameboard as gb...")
 gb = Board(10, 10)
 gb.drawBoard()
-gb.genBoard(Fire)
+gb.genBoard([Fire, Water, Air, Earth])
+gb.drawBoard()
 
 
        
